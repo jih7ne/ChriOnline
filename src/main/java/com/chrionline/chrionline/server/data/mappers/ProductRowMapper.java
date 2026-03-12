76b7cad900bb11f38519226f7179be470edef550
essebaiyayaa <essebaiyaya@gmail.com>
2026-03-12 23:45:05 +0000

    fix(mapper): corriger le mapper Produit pour correspondre au nouveau schéma de la base de données

--- src/main/java/com/chrionline/chrionline/server/data/mappers/ProductRowMapper.java
+++ src/main/java/com/chrionline/chrionline/server/data/mappers/ProductRowMapper.java
@@ -10,11 +10,21 @@ public class ProductRowMapper implements RowMapper<Produit> {
 
     @Override
     public Produit mapRow(ResultSet rs) throws SQLException {
-
         Produit p = new Produit();
-        p.setId(rs.getString("id"));
-        p.setNom(rs.getString("name"));
-        p.setPrix(rs.getDouble("price"));
+        p.setId(rs.getInt("id"));
+        p.setNom(rs.getString("nom"));
+        p.setDescription(rs.getString("description"));
+        p.setPrix(rs.getDouble("prix"));
+        p.setStock(rs.getInt("stock"));
+        p.setUrlImage(rs.getString("url_image"));
+        p.setIdCategorie(rs.getInt("id_categorie"));
+
+        // si on fait un JOIN avec Categorie
+        try {
+            p.setNomCategorie(rs.getString("nom_categorie"));
+        } catch (SQLException e) {
+            // pas de JOIN, on ignore
+        }
 
         return p;
     }
