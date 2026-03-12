81b6d20368010dbc92d2508e67eb1ce58cb69738
essebaiyayaa <essebaiyaya@gmail.com>
2026-03-12 23:44:47 +0000

    feat(model): compléter le modèle PanierProduit avec calcul du sous-total par ligne

--- src/main/java/com/chrionline/chrionline/server/data/models/PanierProduit.java
+++ src/main/java/com/chrionline/chrionline/server/data/models/PanierProduit.java
@@ -1,4 +1,43 @@
 package com.chrionline.chrionline.server.data.models;
 
 public class PanierProduit {
+    private int idPanier;
+    private int idProduit;
+    private int quantite;
+
+    // infos du produit pour affichage
+    private String nomProduit;
+    private double prix;
+    private String urlImage;
+
+    public PanierProduit() {}
+
+    public PanierProduit(int idPanier, int idProduit, int quantite) {
+        this.idPanier = idPanier;
+        this.idProduit = idProduit;
+        this.quantite = quantite;
+    }
+
+    // sous-total de cette ligne
+    public double getSousTotal() {
+        return prix * quantite;
+    }
+
+    public int getIdPanier() { return idPanier; }
+    public void setIdPanier(int idPanier) { this.idPanier = idPanier; }
+
+    public int getIdProduit() { return idProduit; }
+    public void setIdProduit(int idProduit) { this.idProduit = idProduit; }
+
+    public int getQuantite() { return quantite; }
+    public void setQuantite(int quantite) { this.quantite = quantite; }
+
+    public String getNomProduit() { return nomProduit; }
+    public void setNomProduit(String nomProduit) { this.nomProduit = nomProduit; }
+
+    public double getPrix() { return prix; }
+    public void setPrix(double prix) { this.prix = prix; }
+
+    public String getUrlImage() { return urlImage; }
+    public void setUrlImage(String urlImage) { this.urlImage = urlImage; }
 }
