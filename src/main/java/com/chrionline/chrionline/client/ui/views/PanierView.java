b15d6a56143c75b0021b73ac228fe8bd928561de
essebaiyayaa <essebaiyaya@gmail.com>
2026-03-13 12:55:11 +0000

    feat(view): créer PanierView (structure de base)

--- src/main/java/com/chrionline/chrionline/client/ui/views/PanierView.java
+++ src/main/java/com/chrionline/chrionline/client/ui/views/PanierView.java
@@ -1,4 +1,12 @@
 package com.chrionline.chrionline.client.ui.views;
 
-public class PanierView {
+import com.chrionline.chrionline.core.interfaces.ViewManager;
+import com.chrionline.chrionline.network.tcp.TCPClient;
+import javafx.scene.layout.BorderPane;
+import java.util.Map;
+
+public class PanierView extends BorderPane {
+    public PanierView(TCPClient client, Map<String, Object> userData, ViewManager viewManager) {
+        // TODO : implémenter
+    }
 }
