86989041af70808f082a4403c741c1963ed8bb5e
essebaiyayaa <essebaiyaya@gmail.com>
2026-03-13 12:55:24 +0000

    feat(view): créer AdminView (structure de base)

--- src/main/java/com/chrionline/chrionline/client/ui/views/AdminView.java
+++ src/main/java/com/chrionline/chrionline/client/ui/views/AdminView.java
@@ -1,4 +1,12 @@
 package com.chrionline.chrionline.client.ui.views;
 
-public class AdminView {
+import com.chrionline.chrionline.core.interfaces.ViewManager;
+import com.chrionline.chrionline.network.tcp.TCPClient;
+import javafx.scene.layout.BorderPane;
+import java.util.Map;
+
+public class AdminView extends BorderPane {
+    public AdminView(TCPClient client, Map<String, Object> userData, ViewManager viewManager) {
+        // TODO : implémenter
+    }
 }
