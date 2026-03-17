f32d8d5fbbcf1f4884fc2137c99d671f0f187b38
essebaiyayaa <essebaiyaya@gmail.com>
2026-03-17 22:21:19 +0000

    feat: add CategorieRowMapper

---
+++ src/main/java/com/chrionline/chrionline/server/data/mappers/CategorieRowMapper.java
@@ -0,0 +1,19 @@
+package com.chrionline.chrionline.server.data.mappers;
+
+import com.chrionline.chrionline.server.data.RowMapper;
+import com.chrionline.chrionline.server.data.models.Categorie;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CategorieRowMapper implements RowMapper<Categorie> {
+
+    @Override
+    public Categorie mapRow(ResultSet rs) throws SQLException {
+        Categorie c = new Categorie();
+        c.setId(rs.getInt("id"));
+        c.setNom(rs.getString("nom"));
+        c.setDescription(rs.getString("description"));
+        return c;
+    }
+}
