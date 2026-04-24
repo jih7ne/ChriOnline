853efe2d7b11707f9ccb6a811362a0479b35c280
essebaiyayaa <essebaiyaya@gmail.com>
2026-04-24 17:09:15 +0100

    refactor(network/module): update module-info.java

--- NetworkModule/src/main/java/module-info.java
+++ NetworkModule/src/main/java/module-info.java
@@ -2,12 +2,11 @@ module com.chrionline.network {
     requires com.chrionline.sharedmodule;
     requires com.google.gson;
     requires org.slf4j;
-    requires SecurityModule;
+    requires SecurityModule;              // ← back to this
     exports com.chrionline.network.protocol;
     exports com.chrionline.network.tcp;
     exports com.chrionline.network.udp;
     exports com.chrionline.network.enums;
 
     opens com.chrionline.network.protocol to com.google.gson;
-
 }
